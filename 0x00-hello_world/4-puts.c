c313c70d64848a37d06cd086afc1313c3ed6327a
gutawoni <wondishif36@gmail.com>
2022-09-15 07:13:22 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                      
+
+/**                                                                                                                                     
+
+* main - print the string in put function                                                                                               
+
+*                                                                                                                                       
+
+* Description using main function                                                                                                       
+
+* this program prints "Programming is like building a multilingual puzzle                                                               
+
+* Return : 0                                                                                                                            
+
+*/                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+        puts("\"Programming is like building a multilingual puzzle");  
+
 /**
-* main - print the string in put function
-*
-* Description using main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return : 0
-*/
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+*return 0
+
+*/		
+
+        return (0);                                                                                                                     
+
 }
