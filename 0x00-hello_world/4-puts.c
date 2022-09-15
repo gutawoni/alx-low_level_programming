362c787a8924054dbbe929eb9bd477b86cfffd07
gutawoni <wondishif36@gmail.com>
2022-09-15 06:46:38 -0700

    coding fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+
+?* this coding challenge by alx
+
+*
+
+*it returns zero
+
+*finally it neds to run
+
+*/
+
 int main(void)
 
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
 
