21ffdb62d863c1b694feb1e24ca6219ad9bcd16e
gutawoni <wondishif36@gmail.com>
2022-09-15 10:10:26 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,47 @@
+#include <stdio.h>                                                                                                                      
+
+/**                                                                                                                                     
+
+* main - print the string in the puts function                                                                                               
+
+*                                                                                                                                       
+
+* Description using main function                                                                                                       
+
+* this program prints "Programming is like building a multilingual puzzle                                                               
+
+* return : 0                                                                                                                            
+
+*/                                                                                                                                      
+
+int main(void)                                                                                                                          
+
+{            
+
+char c;
+
+int i;
+
+long ll;
+
+long long lll;
+
+float f;                                                                                                                          
+
+        printf("the size of char :id byte(s)\n",sizeof(c) ); 
+
+		printf("the size of char :id byte(s)\n",sizeof(i) );
+
+	    printf("the size of char :id byte(s)\n",sizeof(ll) );
+
+		printf("the size of char :id byte(s)\n",sizeof(lll) );
+
+		printf("the size of char :id byte(s)\n",sizeof(f) );
+
+
+
+        return (0);                                                                                                                     
+
+}
+
+chmod u+x 1	
