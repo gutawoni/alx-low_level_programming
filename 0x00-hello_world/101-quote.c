54930e670e78c826906f21e4ba7d12c2f9877f21
gutawoni <33760699+gutawoni@users.noreply.github.com>
2022-09-22 18:45:52 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,47 +1,13 @@
-#include <stdio.h>                                                                                                                      
+#include <unistd.h>
 
-/**                                                                                                                                     
-
-* main - print the string in the puts function                                                                                               
-
-*                                                                                                                                       
-
-* Description using main function                                                                                                       
-
-* this program prints "Programming is like building a multilingual puzzle                                                               
-
-* return : 0                                                                                                                            
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{            
-
-char c;
-
-int i;
-
-long ll;
-
-long long lll;
-
-float f;                                                                                                                          
-
-        printf("the size of char :id byte(s)\n",sizeof(c) ); 
-
-		printf("the size of char :id byte(s)\n",sizeof(i) );
-
-	    printf("the size of char :id byte(s)\n",sizeof(ll) );
-
-		printf("the size of char :id byte(s)\n",sizeof(lll) );
-
-		printf("the size of char :id byte(s)\n",sizeof(f) );
-
-
-
-        return (0);                                                                                                                     
-
+/**
+* main - Prints a text
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar,
+2015-10-19\n", 59);
+return (1);
 }
-
-chmod u+x 1	
