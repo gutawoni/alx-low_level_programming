d2c7373afa78fbc1b74d3242ec977da7599a46f9
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2022-12-17 15:03:24 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,8 @@
   */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	int _putchar(char c)
+{
+	return (write(1, &c, 1));
 
-	return (0);
 }
